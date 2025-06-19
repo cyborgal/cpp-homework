6be810dbe57b873daff79ec5717cc2f431662288
Baelyn Thantron <123280044+cyborgal@users.noreply.github.com>
2025-06-19 16:11:24 -0700

    Create assignment4.cpp

---
+++ assignment4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    float n1, n2, n3;
+
+    cout << "Enter three numbers: ";
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+
+    if (n1 >= n2 && n1 >= n3) {
+        cout << "Largest number: " << n1;
+    }
+         
+    if (n2 >= n1 && n2 >= n3) {     
+        cout << "Largest number: " << n2;
+    }
+    
+    if (n3 >= n1 && n3 >= n2) {
+        cout << "Largest number: " << n3;
+    }
+    return 0;
+}
